33c84b8cd0764456b40bfbd402c09c06a99f735d
v-Gamal Fajri <v-Gamal.Fajri@btpn.com>
2021-06-13 22:09:30 +0700

    first commit

---
+++ auth/auth.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	commonResponse "github.com/gmlalfjr/go_CommonResponse/utils"
+)
+
+func VerifyAuthorization(jwtSecret string) {
+	var c *gin.Context = &gin.Context{}
+	getHeader := c.GetHeader("Authorization")
+	if len(getHeader) <= 0 {
+		c.JSON(http.StatusBadRequest, commonResponse.NewBadRequest("Bad request"))
+		c.Abort()
+		return
+	}
+	if !strings.Contains(getHeader, "Bearer") {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": string("Invalid Token"),
+		})
+		c.Abort()
+		return
+	}
+	tokenString := strings.Replace(getHeader, "Bearer ", "", -1)
+	fmt.Println(tokenString, "token string")
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecret), nil
+	})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	for key, val := range claims {
+		c.Set(key, val)
+	}
+	c.Next()
+}
